8eecd83599f9a680494e55f591c3e02b0619dbfa
Olivier Lelièvre <ladressemaildolivier@gmail.com>
2024-10-28 17:00:48 +0100

    Responsive OK

--- wp-content/themes/foce-child/parts/nominationOscars.php
+++ wp-content/themes/foce-child/parts/nominationOscars.php
@@ -1,9 +1,9 @@
 <section id=oscars>
     <div class="container">
-        <div>
+        <div >
             <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
             <img src="<?php echo get_stylesheet_directory_uri() . '/images/orange_oscars_bg.png'; ?>" alt="">
         </div>
-            <img class="logoOscars" src="<?php echo get_stylesheet_directory_uri() . '/images/LogoOscars.png'; ?>" alt="logo des Oscars Short Film Animated">
+        <img class="logoOscars" src="<?php echo get_stylesheet_directory_uri() . '/images/LogoOscars.png'; ?>" alt="logo des Oscars Short Film Animated">
     </div>
 </section>
