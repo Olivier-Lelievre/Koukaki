8a5701aca1c36393ae9365865d9f6c1d3d004ae0
Olivier Lelièvre <ladressemaildolivier@gmail.com>
2024-10-28 09:31:41 +0100

    Burger OK aler et retour

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -1,105 +1,68 @@
 // BURGER
+
 const menu = document.querySelector('.burger-open');
 const burger = document.querySelector('#site-navigation ul button');
 
 burger.addEventListener('click', () => {
-  menu.classList.toggle('active');
+  menu.classList.toggle('open');
   burger.classList.toggle('crossBurger')
 });
 
 document.querySelectorAll(".link-burger").forEach(n => n.addEventListener("click", () => {
-  menu.classList.remove("active");
+  menu.classList.remove("open");
   burger.classList.remove('crossBurger')
 }));
-
-
 
 // ANIMATION TITRES H2
 
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        // Vérifie quel élément est observé et applique une classe différente en fonction
-        if (entry.target.classList.contains('title-delay-1')) {
-          entry.target.classList.add('animation-titles', 'delay-1');
-        } else if (entry.target.classList.contains('title-delay-2')) {
-          entry.target.classList.add('animation-titles', 'delay-2');
-        } else {
-          entry.target.classList.add('animation-titles');
-        }
-      } else {
-        entry.target.classList.remove('animation-titles', 'delay-1', 'delay-2');
+        entry.target.classList.add('animation-titles');
+        return;
       }
+      entry.target.classList.remove('animation-titles');
     });
-  }, {
-    rootMargin: '0px 0px 50px 0px' // Modifie la zone du viewport pour déclencher l'animation avant que l'élément ne touche le bord inférieur
   });
-  
-  // Sélectionner tous les éléments .title (les 3 spans)
-  const titles = document.querySelectorAll('.title');
-  
-  // Observer tous les éléments .title
+  const titles = document.querySelectorAll('span');
   titles.forEach((element) => observer.observe(element));
-  
 
 // NUAGES
 
-// Ajout d'un écouteur d'événement qui détecte le scroll de la fenêtre.
 window.addEventListener('scroll', function () {
-
-  // Selection des éléments HTML avec les classes
   const bigCloud = document.querySelector('.clouds-big');
   const littleCloud = document.querySelector('.clouds-little');
   const placeSection = document.querySelector('#place');
 
-  // Récupère la position verticale de l'élément avec l'id 'place' par rapport au haut de la page.
   const sectionOffsetTop = placeSection.offsetTop;
-  // Obtenez la position actuelle de défilement de la fenêtre.
   const scrollPosition = window.scrollY;
 
-  // Vérifie si la position verticale de défilement a atteint ou est supérieur à la position de l'élément avec l'id 'place'
   if (scrollPosition >= sectionOffsetTop) {
-
-  // Calculez la valeur de parallaxe, qui est la différence entre la position de défilement et le haut relatif, divisée par 4.
-      
-      //Cette différence représente la distance parcourue dans le défilement par rapport à l'élément #place.
-      //La valeur résultante est ensuite divisée par 4 pour ralentir l'effet de défilement des nuages.
-      //Si cette division n'était pas présente, les nuages se déplaceraient trop rapidement.
       const parallaxValue = (scrollPosition - sectionOffsetTop) / 4;
-
-      //Cette fonction renvoie la plus petite valeur entre parallaxValue (calculé précédemment) et 300.
-      //Le déplacement des nuages sera limité à 300 pixels.
       const translationX = Math.min(parallaxValue, 300);
-
-      //Cette ligne de code déplace horizontalement l'élément bigCloud vers la gauche de "translationX" pixels.
       bigCloud.style.transform = 'translateX(' + (-translationX) + 'px)';
       littleCloud.style.transform = 'translateX(' + (-translationX) + 'px)';
-      //littleCloud.style.left = (scrollPosition - sectionOffsetTop) * 1 + 'px';
-      //littleCloud.classList.add("animCloud");
-      //littleCloud.style.transform = `translateX(-${(scrollPosition - sectionOffsetTop) * 0.5}px)`;
     }
 });
 
+// SKROLLR
 
+var s = skrollr.init();
 
-    
-   /* const movingImage = document.querySelector(".clouds .clouds-little");
-        
-    // Valeurs limites pour le défilement
-    const scrollMin = 900;  // Position min de défilement pour commencer à bouger l'image
-    const scrollMax = 2300;  // Position max de défilement pour arrêter de bouger l'image
+// SWIPER
 
-    // Fonction de gestion du défilement
-    function handleScroll() {
-        const scrollPosition = window.scrollY;
-
-        // Vérifier si la position de défilement est entre scrollMin et scrollMax
-        if (scrollPosition >= scrollMin && scrollPosition <= scrollMax) {
-            // Déplacer l'image en fonction du défilement
-            movingImage.style.left = (scrollPosition - scrollMin) * 1 + 'px';
-        }
-    }
-
-    // Écoute l'événement de défilement
-    window.addEventListener('scroll', handleScroll); */
-
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loopAddBlankSlides: false,
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+});
--- wp-content/themes/foce-child/parts/nominationOscars.php
+++ wp-content/themes/foce-child/parts/nominationOscars.php
@@ -1,6 +1,9 @@
 <section id=oscars>
     <div class="container">
+        <div>
             <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
-            <img class="logoOscars" src="wp-content/themes/foce-child/images/LogoOscars.png" alt="logo des Oscars Short Film Animated">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/images/orange_oscars_bg.png'; ?>" alt="">
+        </div>
+            <img class="logoOscars" src="<?php echo get_stylesheet_directory_uri() . '/images/LogoOscars.png'; ?>" alt="logo des Oscars Short Film Animated">
     </div>
 </section>
